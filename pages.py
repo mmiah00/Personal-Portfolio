97f84f7fc8f374b0012daeafd0e76f2cd3c248ee
mmiah <mmiah@bu.edu>
2022-06-02 13:54:11 -0400

    make layout for the different pages I'll add to the website

--- pages.py
+++ pages.py
@@ -4,3 +4,23 @@ app = Flask (__name__)
 @app.route("/")
 def homepage(): 
 	return 'Welcome to My Portfolio!'
+
+@app.route("/aboutme")
+def aboutMe(): 
+	return 'Learn about me'
+
+@app.route("/experience")
+def workExp(): 
+	return 'Here are some things I've worked on'
+
+@app.route("/hobbies")
+def hobbies(): 
+	return 'Some things I like to do'
+
+@app.route("/education")
+def education(): 
+	return 'Education:' 
+
+@app.route("/myMap")
+def myMap(): 
+	return 'Some places I've been to' 
