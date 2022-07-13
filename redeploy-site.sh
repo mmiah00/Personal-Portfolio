8021122c09552d889e9024fd53cdc04b0c55bc5b
mmiah00 <maishm357@gmail.com>
2022-07-13 20:14:57 +0000

    edited deployment script to deploy using docker containers

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,11 +1,5 @@
 #!/bin/bash  
 cd ~/Portfolio-Project
 git fetch && git reset origin/main --hard 
-source python3-virtualenv/bin/activate   
-pip install -r requirements.txt                        
-
-systemctl start myportfolio
-systemctl enable myportfolio 
-
-systemctl daemon-reload 
-systemctl restart myportfolio 
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
