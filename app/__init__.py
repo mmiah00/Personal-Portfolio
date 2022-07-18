442c68d17828df7f5d2857d3fcc118971cdbaf06
mmiah <mmiah@bu.edu>
2022-07-18 13:24:03 -0400

    trying new way of writing delete

--- app/__init__.py
+++ app/__init__.py
@@ -115,21 +115,30 @@ def get_time_line_post():
 #         from traceback import format_exec
 #         print(format_exec())
 
-@app.route("/api/timeline_post/<id>", methods=['DELETE'])
+@app.route('/api/timeline_post/<int:id>', methods=['DELETE'])
 def delete_timeline_post(id):
-    toDelete = TimelinePost.query.get(id)
-    mydb.session.delete(toDelete)
-    mydb.session.commit()
+#     if not testing: mydb.connect()
+    post_to_delete = TimelinePost.get(TimelinePost.id == id)
+    deleted_post = model_to_dict(post_to_delete)
+    post_to_delete.delete_instance()
+#     if not testing: mydb.close()
 
-    return { 
-		'timeline_posts': [
-			model_to_dict(p)
-			for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
-		]
-	}
+    return deleted_post
+    
+# @app.route("/api/timeline_post/<id>", methods=['DELETE'])
+# def delete_timeline_post(id):
+#     toDelete = TimelinePost.query.get(id)
+#     mydb.session.delete(toDelete)
+#     mydb.session.commit()
 
-    # return "Post was successfully deleted"
+#     return { 
+# 		'timeline_posts': [
+# 			model_to_dict(p)
+# 			for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
+# 		]
+# 	}
 
+#     # return "Post was successfully deleted"
 
 # @app.route('/api/timeline_post/<int:nid>', methods=['DELETE'])
 # def delete_time_line_post(nid): 
