d5d96c5e6e0ea8c708b3b5e849d018aedf30e163
mmiah <mmiah@bu.edu>
2023-06-08 19:40:02 -0400

    got inspiration from https://youtu.be/kmM6mqvnxcs, made layout of website

---
+++ app/templates/script.js
@@ -0,0 +1,16 @@
+let text = document.getElementById('text'); 
+let leaf = document.getElementById('leaf'); 
+let hill1 = document.getElementById('hill1'); 
+let hill4 = document.getElementById('hill4'); 
+let hill5 = document.getElementById('hill5'); 
+
+window.addEventListener('scroll', () => { 
+    let value = window.scrollY; 
+
+    text.style.marginTop = value * 2.5  + 'px'; // makes title move down until it is gone behind the bottom hills  
+    leaf.style.top = value * -1.5 + 'px'; 
+    leaf.style.left = value * 1.5 + 'px'; //lines 11 and 12 together make the leaf in the top right corner move diagonally northeast
+    hill5.style.left = value * 1.5 + 'px'; // makes hill on the right side move right until it is out of the screen
+    hill4.style.left = value * -1.5 + 'px'; // makes hill on the left side move left until out of the screen 
+    hill1.style.top = value * -1.5 + 'px'; // makes hills on top go down as text moves down 
+}); 
